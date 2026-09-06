f0802c48bddde842e0ba40cdd0cd9a822c883451
Phan Ngoc Vinh <vinhngoc125426@gmail.com>
2026-09-06 16:44:45 +0700

    BT vong lap bt3

---
+++ VL_Bt3.kt
@@ -0,0 +1,13 @@
+fun main() {
+    var i = 1
+
+    println("Sinh viên: Phan Ngọc Vinh")
+    println("MSSV: 2415053122249")
+
+    while (i <= 20) {
+        if (i % 2 == 0) {
+            println(i)
+        }
+        i++
+    }
+}
