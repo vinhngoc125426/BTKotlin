aa3bbb703989ee1b06274cf5e974628a0226c487
Phan Ngoc Vinh <vinhngoc125426@gmail.com>
2026-09-06 16:44:07 +0700

    BT vong lap bt2

---
+++ VL_Bt2.kt
@@ -0,0 +1,11 @@
+fun main() {
+    var sum = 0
+
+    println("Sinh viên: Phan Ngọc Vinh")
+    println("MSSV: 2415053122249")
+    for (i in 1..100) {
+        sum += i
+    }
+
+    println("Tổng = $sum")
+}
